0a53242fd4686c78b01dfcac70f72bb4308af96a
Mochammad Alwi <alwi89@gmail.com>
2021-09-08 02:59:39 +0700

    iframe pnpb dan sub2nya

--- src/embed/IframeGenerator.jsx
+++ src/embed/IframeGenerator.jsx
@@ -4,8 +4,21 @@ import ReactDOM from "react-dom";
 import RealisasiAnggaran from "../pages/pnbp/sie-pnbp-realisasi-anggaran";
 import PaguMp from "../pages/pnbp/sie-pnbp-pagu-mp.js";
 import PaguMpOpsNon from "../pages/pnbp/sie_pnbp_pagu_mp_ops_non";
+import PengembalianPNBP from "../pages/pnbp/sie-pengembalian-pnbp";
+import PeringkatRealisasi from "../pages/pnbp/sie-peringkat-realisasi";
+import AlokasiAnggaran from "../pages/pnbp/sie-pnbp-alokasi-anggaran";
+import PnbpBerkasPeringkatWilayah from "../pages/pnbp/sie-pnbp-berkas-peringkat-wilayah";
+import PnbpBerkasPeringkat from "../pages/pnbp/sie-pnbp-berkas-peringkat";
+import PnbpBerkasWilayah from "../pages/pnbp/sie-pnbp-berkas-wilayah";
+import KinerjaBerkasPnbp from "../pages/pnbp/sie-pnbp-kinerja-berkas";
+import PersentaseRealisasiBelanja from "../pages/pnbp/sie-pnbp-persentase-realisasi-belanja";
+import RealisasiPenerimaan from "../pages/pnbp/sie-pnbp-realisasi-penerimaan";
+import RealisasiPenggunaan from "../pages/pnbp/sie-pnbp-realisasi-pengunaan";
+import RealisasiTargetPenerimaan from "../pages/pnbp/sie-pnbp-realisasi-target-penerimaan";
 import SiePsnPtsl5Peringkat from "../pages/psn/PTSL/sie_psn_ptsl_5peringkat";
 import SieLayananJumlahPerjenis from "../pages/kinerjaLayanan/layananUmum/sie_layanan_jumlah_perjenis";
+import { Provider } from "react-redux";
+import store from "../config/store";
 
 class IframeGenerator extends React.Component {
   constructor(props) {
@@ -28,6 +41,82 @@ class IframeGenerator extends React.Component {
       case "sie-pnbp-pagu-mp-ops-non":
         ReactDOM.render(
           <PaguMpOpsNon />,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pengembalian-pnbp":
+        ReactDOM.render(
+          <Provider store={store}>
+            <PengembalianPNBP />
+          </Provider>,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-peringkat-realisasi":
+        ReactDOM.render(
+          <PeringkatRealisasi />,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-alokasi-anggaran":
+        ReactDOM.render(
+          <AlokasiAnggaran />,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-berkas-peringkat-wilayah":
+        ReactDOM.render(
+          <PnbpBerkasPeringkatWilayah />,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-berkas-peringkat":
+        ReactDOM.render(
+          <PnbpBerkasPeringkat />,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-berkas-wilayah":
+        ReactDOM.render(
+          <Provider store={store}>
+            <PnbpBerkasWilayah />
+          </Provider>,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-kinerja-berkas":
+        ReactDOM.render(
+          <Provider store={store}>
+            <KinerjaBerkasPnbp />
+          </Provider>,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-persentase-realisasi-belanja":
+        ReactDOM.render(
+          <Provider store={store}>
+            <PersentaseRealisasiBelanja />
+          </Provider>,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-realisasi-penerimaan":
+        ReactDOM.render(
+          <RealisasiPenerimaan />,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-realisasi-penggunaan":
+        ReactDOM.render(
+          <Provider store={store}>
+            <RealisasiPenggunaan />
+          </Provider>,
+          document.getElementById("replace-tag")
+        );
+        break;
+      case "sie-pnbp-realisasi-target-penerimaan":
+        ReactDOM.render(
+          <RealisasiTargetPenerimaan />,
           document.getElementById("replace-tag")
         );
         break;
--- src/pages/pnbp/sie-peringkat-realisasi.js
+++ src/pages/pnbp/sie-peringkat-realisasi.js
@@ -58,6 +58,11 @@ import moment from "moment";
 import { fileExport } from "../../functionGlobal/exports";
 import { loadDataColumnTable } from "../../functionGlobal/fileExports";
 import { useHistory } from "react-router-dom";
+import { BASE_URL } from "../../config/embed_conf";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const dataTemp = [
   {
     ranking: 1,
@@ -439,8 +444,250 @@ const PeringkatRealisasi = () => {
     });
   };
 
+  const [iframeIsOpen, setOpenIframe] = useState(false);
+  const [iframeWidth, setIframeWidth] = useState(600);
+  const [iframeHeight, setIframeHeight] = useState(600);
+
+  function handleIframe(status) {
+    setOpenIframe(status);
+  }
+
+  function handleIframeWidth(e) {
+    // alert(e.target.value + "");
+    setIframeWidth(e.target.value);
+  }
+
   return (
     <div>
+      <Modal
+        open={iframeIsOpen}
+        // onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        // className={classes.modalStyle1}
+        style={{
+          // display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          overflow: "scroll",
+          width: "90%",
+          height: "98%",
+          padding: "0 9em 0em 5em",
+        }}
+      >
+        <div className={classes.paper}>
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            style={
+              {
+                // padding: 10,
+                // paddingTop: 20,
+                // paddingBottom: 5,
+              }
+            }
+          >
+            <Grid
+              container
+              direction="row"
+              // justifyContent="flex-center"
+              alignItems="flex-start"
+              item
+              xs={10}
+            >
+              <h2 id="simple-modal-title" style={{ paddingBottom: 20 }}>
+                Embeded Code Generator
+              </h2>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              // justifyContent="flex-center"
+              alignItems="flex-start"
+              item
+              xs={1}
+            >
+              <CopyToClipboard
+                text={
+                  '<iframe width="' +
+                  iframeWidth +
+                  '" height="' +
+                  iframeHeight +
+                  '"' +
+                  ' src="' +
+                  BASE_URL.domain +
+                  "/embed/" +
+                  BASE_URL.path.peringkat_realisasi +
+                  '"></iframe>'
+                }
+                onCopy={() => toast.success("success copied to clipboard!")}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ height: 30, width: "100%" }}
+                >
+                  Copy
+                </Button>
+              </CopyToClipboard>
+              <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+              />
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              // justifyContent="flex-center"
+              alignItems="flex-start"
+              item
+              xs={1}
+            >
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => setOpenIframe(false)}
+                style={{ height: 30, width: "100%" }}
+              >
+                Close
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            style={{
+              // padding: 10,
+              // paddingTop: 20,
+              paddingBottom: 5,
+            }}
+          >
+            <Grid
+              container
+              direction="row"
+              // justifyContent="flex-center"
+              alignItems="flex-start"
+              item
+              xs={10}
+            >
+              <textarea
+                style={{ width: "100%" }}
+                rows={3}
+                value={
+                  '<iframe width="' +
+                  iframeWidth +
+                  '" height="' +
+                  iframeHeight +
+                  '"' +
+                  ' src="' +
+                  BASE_URL.domain +
+                  "/embed/" +
+                  BASE_URL.path.peringkat_realisasi +
+                  '"></iframe>'
+                }
+              />
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              // justifyContent="flex-start"
+              alignItems="flex-start"
+              item
+              xs={2}
+            >
+              <Grid
+                container
+                direction="row"
+                // justifyContent="flex-start"
+                alignItems="flex-start"
+                item
+              >
+                <Grid
+                  direction="row"
+                  // justifyContent="flex-end"
+                  alignItems="flex-center"
+                  item
+                  xs={4}
+                >
+                  width
+                </Grid>
+                <Grid
+                  direction="row"
+                  // justifyContent="flex-end"
+                  alignItems="flex-center"
+                  item
+                  xs={8}
+                >
+                  <input
+                    type="number"
+                    value={iframeWidth}
+                    style={{ width: "100%" }}
+                    onChange={(e) => setIframeWidth(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                // justifyContent="flex-start"
+                alignItems="flex-start"
+                item
+              >
+                <Grid
+                  direction="row"
+                  // justifyContent="flex-end"
+                  alignItems="flex-center"
+                  item
+                  xs={4}
+                >
+                  height
+                </Grid>
+                <Grid
+                  direction="row"
+                  // justifyContent="flex-end"
+                  alignItems="flex-center"
+                  item
+                  xs={8}
+                >
+                  <input
+                    type="number"
+                    value={iframeHeight}
+                    style={{ width: "100%" }}
+                    onChange={(e) => setIframeHeight(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            style={{
+              // padding: 10,
+              // paddingTop: 20,
+              paddingBottom: 5,
+            }}
+          >
+            <iframe
+              width={iframeWidth}
+              height={iframeHeight}
+              src={
+                BASE_URL.domain + "/embed/" + BASE_URL.path.peringkat_realisasi
+              }
+            ></iframe>
+          </Grid>
+        </div>
+      </Modal>
       <Modal
         open={open}
         onClose={handleClose}
@@ -488,15 +735,7 @@ const PeringkatRealisasi = () => {
               <IconButton
                 size="small"
                 onClick={() => {
-                  navigator.clipboard.writeText(
-                    '<iframe width="500" height="500"' +
-                      ' src="' +
-                      BASE_URL.domain +
-                      "/embed/" +
-                      BASE_URL.path.peringkat_realisasi +
-                      '"></iframe>'
-                  );
-                  alert("code embeded berhasil dicopy");
+                  handleIframe(true);
                 }}
               >
                 <IoCopySharp />
